409795b0a0e0fdf4724ed7b0cc30aa91e86b7c36
= <=>
2025-01-17 11:41:57 +0530

    Test Cmmit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("from /")
+    res.send("Hi This is test version 1")
+})
+
+app.get('/health', (req, res) => {
+    console.log("from Health")
+    res.status(200).send({"message": "Its seems good over here"})
+})
+
+app.listen(3000, () => {
+    console.log('Server is Listening at port 3000')
+})
+
